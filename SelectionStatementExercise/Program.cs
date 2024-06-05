4cbd234ca67102cc58d5a7591954868fcdcb4505
Aaron Carlson <acarlson2@carthage.edu>
2024-06-05 10:49:11 -0500

    exercise completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNum = 260;
+            Console.WriteLine("Try to guess my favorite number.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favNum)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if(userInput > favNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine("What is your favorite school subject?");
+            var userInput2 = Console.ReadLine();
+
+            switch(userInput2)
+            {
+                case "Math":
+                    Console.WriteLine($"{userInput2}? Not my favorite subject, but good for you.");
+                    break;
+                case "Science":
+                    Console.WriteLine($"{userInput2}? I love {userInput2}!");
+                    break;
+                case "History":
+                    Console.WriteLine($"{userInput2}? That's my favorite subject too!");
+                    break;
+                case "English":
+                    Console.WriteLine($"{userInput2}? Personally, I find that subject kinda boring.");
+                    break;
+                case "PE":
+                    Console.WriteLine($"{userInput2}? That's understandable.");
+                    break;
+                default:
+                    Console.WriteLine($"{userInput2}? I've never heard of that subject before.");
+                    break;
+            }
         }
     }
 }
